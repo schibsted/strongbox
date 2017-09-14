75f8924c52dc4753816a463f7b3e2526dcecee66
Adam Števko <adam.stevko@gmail.com>
2017-09-14 13:20:44 +0200

    Add integration with Travis

---
+++ script/generate-bintray-json.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#
+# Script to generate descriptor.json file needed by Travis for bintray deployment 
+#
+
+set -e
+
+populate_descriptor_file() {
+	local package_type=$1
+	
+	local tmpl_filename="descriptor-${package_type}.json.tmpl"
+	local input_file="${ROOT_DIR}/script/${tmpl_filename}"
+	local output_file="${DEST_DIR}//${tmpl_filename%.tmpl}"
+
+	sed -e "s/@DATE@/${DATE}/g" \
+	    -e "s/@PKG_VERSION@/${TRAVIS_TAG}/g" \
+	    -e "s/@BINTRAY_USER@/${BINTRAY_USER}/g" \
+	    ${input_file} > ${output_file} 
+}
+
+if [[ ! -n "${TRAVIS_TAG}" ]]; then
+	echo 'No $TRAVIS_TAG environment variable found!'
+	exit 1
+fi 
+
+export ROOT_DIR=$(cd $(dirname $0)/..; pwd)
+export DEST_DIR="${ROOT_DIR}/bintray"
+export DATE=$(date +%Y-%m-%d)
+
+[[ -d "${DEST_DIR}" ]] || mkdir -p "${DEST_DIR}"
+
+populate_descriptor_file deb
+populate_descriptor_file rpm
+populate_descriptor_file tar
