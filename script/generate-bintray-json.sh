8daab60496b1fd82d599ed03e170e2c88ec52cb6
Adam Števko <xen0l@users.noreply.github.com>
2017-09-19 16:30:28 +0200

    Add signing for RPM/DEB packages (#5)

--- script/generate-bintray-json.sh
+++ script/generate-bintray-json.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #
-# Script to generate descriptor.json file needed by Travis for bintray deployment 
+# Script to generate descriptor.json file needed by Travis for bintray deployment
 #
 
 set -e
 
 populate_descriptor_file() {
 	local package_type=$1
-	
+
 	local tmpl_filename="descriptor-${package_type}.json.tmpl"
 	local input_file="${ROOT_DIR}/script/${tmpl_filename}"
 	local output_file="${DEST_DIR}//${tmpl_filename%.tmpl}"
@@ -15,13 +15,8 @@ populate_descriptor_file() {
 	sed -e "s/@DATE@/${DATE}/g" \
 	    -e "s/@PKG_VERSION@/${TRAVIS_TAG}/g" \
 	    -e "s/@BINTRAY_USER@/${BINTRAY_USER}/g" \
-	    ${input_file} > ${output_file} 
+	    ${input_file} > ${output_file}
 }
-
-if [[ ! -n "${TRAVIS_TAG}" ]]; then
-	echo 'No $TRAVIS_TAG environment variable found!'
-	exit 1
-fi 
 
 export ROOT_DIR=$(cd $(dirname $0)/..; pwd)
 export DEST_DIR="${ROOT_DIR}/bintray"
