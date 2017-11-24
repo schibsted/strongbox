27e620be1c4c3bd169582a2bb5d4afaabc5c86fd
Stian Kristoffersen <stiankri@acm.org>
2017-11-24 15:01:07 +0100

    Change license (#32)

--- cli/src/main/java/com/schibsted/security/strongbox/gui/Singleton.java
+++ cli/src/main/java/com/schibsted/security/strongbox/gui/Singleton.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 Schibsted Products & Technology AS. Licensed under the terms of the MIT license. See LICENSE in the project root.
+ * Copyright (c) 2016 Schibsted Products & Technology AS. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
  */
 
 package com.schibsted.security.strongbox.gui;
