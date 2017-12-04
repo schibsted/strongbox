119e6691fbeeceb7f9ef85f29c0007adf3095778
Stian Kristoffersen <stian.kristoffersen@schibsted.com>
2017-12-04 18:01:07 +0100

    Debug for homebrew release

--- script/deploy-homebrew-formula.sh
+++ script/deploy-homebrew-formula.sh
@@ -5,16 +5,32 @@
 
 set -e
 
-WORKING_DIR=$(cd $(dirname $0)/..; pwd)/homebrew/git
+WORKING_DIR=$(cd $(dirname $0)/..; pwd)/homebrew
+GIT_DIR=${WORKING_DIR}/git
+HOMEBREW_FORMULA=${WORKING_DIR}/strongbox.rb
 
-[[ -d "${WORKING_DIR}" ]] || mkdir -p "${WORKING_DIR}"
+[[ -d "${GIT_DIR}" ]] || mkdir -p "${GIT_DIR}"
 
 GIT_REPO=https://${GITHUB_TOKEN}@github.com/schibsted/homebrew-strongbox.git
 
-cd ${WORKING_DIR}
+cd ${GIT_DIR}
 git init
+
+echo "Working dir:"
+pwd
+
 git pull ${GIT_REPO}
-cp ../strongbox.rb .
+
+if [ ! -f ${HOMEBREW_FORMULA} ]; then
+    echo "Homebrew formula not present in '${HOMEBREW_FORMULA}'"
+    exit 1
+fi
+
+cp ${HOMEBREW_FORMULA} .
 git add strongbox.rb
+
+echo "Working dir:"
+pwd
+
 git commit -m "Upgraded to version ${TRAVIS_TAG}"
 git push ${GIT_REPO} master
