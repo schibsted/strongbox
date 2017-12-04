48b2aef0490c109ed57cd97c5749f2820de9c06d
Samuel Gulliksson <samuel.gulliksson@schibsted.com>
2017-12-04 13:37:26 +0100

    Handle missing secret in InMemoryPlaintextSecret.
    Simplify the implementation by using existing functionality from
    StringDerivedProperty to decrypt the secret each time it changes.

--- archaius/src/main/java/com/schibsted/security/strongbox/archaius/InMemoryPlaintextSecretDerived.java
+++ archaius/src/main/java/com/schibsted/security/strongbox/archaius/InMemoryPlaintextSecretDerived.java
@@ -19,6 +19,7 @@ import java.util.function.Function;
  */
 public class InMemoryPlaintextSecretDerived<T> extends StringDerivedProperty<T> {
     public InMemoryPlaintextSecretDerived(SecretsGroup secretsGroup, SecretIdentifier secretIdentifier, Function<String, T> decoder) {
+        // Please note: null is used as the default value, as it generally does not make sense to have a default secret
         super(secretsGroup.srn(secretIdentifier).toSrn(),
                 null,
                 value -> decoder.apply(DecryptSecret.fromJsonBlob(value, secretsGroup, secretIdentifier)));
