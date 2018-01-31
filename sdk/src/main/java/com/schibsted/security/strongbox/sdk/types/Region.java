e21fb26a60cf5c1ab60369fd4ac6f409da6724c3
emil <emil.vaagland@schibsted.com>
2018-01-31 16:09:39 +0100

    Update Region.java

--- sdk/src/main/java/com/schibsted/security/strongbox/sdk/types/Region.java
+++ sdk/src/main/java/com/schibsted/security/strongbox/sdk/types/Region.java
@@ -16,16 +16,22 @@ public enum Region {
 
     GovCloud("us-gov-west-1"),
     US_EAST_1("us-east-1"),
+    US_EAST_2("us-east-2"),
     US_WEST_1("us-west-1"),
     US_WEST_2("us-west-2"),
     EU_WEST_1("eu-west-1"),
+    EU_WEST_2("eu-west-2"),
+    EU_WEST_3("eu-west-3"),
     EU_CENTRAL_1("eu-central-1"),
+    AP_SOUTH_1("ap-south-1"),
     AP_SOUTHEAST_1("ap-southeast-1"),
     AP_SOUTHEAST_2("ap-southeast-2"),
     AP_NORTHEAST_1("ap-northeast-1"),
     AP_NORTHEAST_2("ap-northeast-2"),
     SA_EAST_1("sa-east-1"),
     CN_NORTH_1("cn-north-1"),
+    CN_NORTHWEST_1("cn-northwest-1"),
+    CA_CENTRAL_1("ca-central-1"),
     ;
 
     private static Map<String, Region> regionMap = new HashMap();
